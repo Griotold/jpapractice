bdbfcf23f81b5941937a72e87612267f363d765c
Griotold <gotjd9773@naver.com>
2022-12-02 13:06:11 +0900

    연관관계 매핑 기초 - 단방향 연관관계

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Team;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -16,6 +19,23 @@ public class JpaMain {
 
         tx.begin();
         try{
+            // 팀 저장
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            // 멤버 저장
+            Member member = new Member();
+            member.setName("memA");
+            member.setTeam(team); //단방향 연관관계 설정, 참조 저장
+            em.persist(member);
+
+            //조회
+            Member findMember = em.find(Member.class, member.getId());
+            System.out.println("findMember = " + findMember.getName());
+            //참조를 사용해서 연관관계 조회
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam = " + findTeam.getName());
 
             tx.commit();
         } catch(Exception e){
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,23 +1,30 @@
 package jpabook.jpashop.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Setter @Getter
 public class Member {
     @Id
     @GeneratedValue // 기본값이 Auto
     @Column(name="member_id")
     private Long id;
+
+    @Column(name="username")
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+//    private String city;
+//    private String street;
+//    private String zipcode;
 }
